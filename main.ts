fb5b4142539556b722b7e10591c34f29a3875172
elsitlte <1570056132@qq.com>
2018-12-14 17:18:14 +0800

    initial

--- main.ts
+++ main.ts
@@ -13,243 +13,58 @@
  * The most important macro "block" specifies that a
  * block should be generated for a **exported** function.
  */
-//% color="#AAc044" icon="\uf06e" block="VisualRecognition"
+//% color="#5B80A5" icon="\uf06e" block="VisualRecognition"
 namespace visualRecognition {
 
-    export enum Servos {
-        S1 = 0x01,
-        S2 = 0x02,
-        S3 = 0x03,
-        S4 = 0x04,
-        S5 = 0x05,
-        S6 = 0x06,
-        S7 = 0x07,
-        S8 = 0x08
-    }
-
-    export enum Motors {
-        MS1 = 0x1,
-        MS2 = 0x2,
-        MS3 = 0x3,
-        MS4 = 0x4
-    }
-
-    export enum Steppers {
-        //% blockId="Step_1" block="Step1"
-        Step_1 = 0x1,
-        //% blockId="Step_2" block="Step2"
-        Step_2 = 0x2
-    }
-
-    export enum Turns {
-        //% blockId="T1B4" block="1/4"
-        T1B4 = 90,
-        //% blockId="T1B2" block="1/2"
-        T1B2 = 180,
-        //% blockId="T1B0" block="1"
-        T1B0 = 360,
-        //% blockId="T2B0" block="2"
-        T2B0 = 720,
-        //% blockId="T3B0" block="3"
-        T3B0 = 1080,
-        //% blockId="T4B0" block="4"
-        T4B0 = 1440,
-        //% blockId="T5B0" block="5"
-        T5B0 = 1800
-    }
-    
-    export enum Directions {
-       //% blockId="Forward" block="0"
-       Forward,
-       //% blockId="Backward" block="1"
-       Backward        
-    }
-    
-    let initialized = false
-    let initializedMatrix = false
-
-    let matBuf = pins.createBuffer(17);
-    let distanceBuf = 0;
-
-    function i2cwrite(addr: number, reg: number, value: number) {
-        let buf = pins.createBuffer(2)
-        buf[0] = reg
-        buf[1] = value
-        pins.i2cWriteBuffer(addr, buf)
-    }
-
-    function i2ccmd(addr: number, value: number) {
-        let buf = pins.createBuffer(1)
-        buf[0] = value
-        pins.i2cWriteBuffer(addr, buf)
-    }
-
-    function i2cread(addr: number, reg: number) {
-        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
-        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
-        return val;
-    }
-
-    function initPCA9685(): void {
-        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
-        setFreq(50);
-        for (let idx = 0; idx < 16; idx++) {
-            setPwm(idx, 0 ,0);
-        }
-        initialized = true
-    }
-
-    function setFreq(freq: number): void {
-        // Constrain the frequency
-        let prescaleval = 25000000;
-        prescaleval /= 4096;
-        prescaleval /= freq;
-        prescaleval -= 1;
-        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
-        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
-        let newmode = (oldmode & 0x7F) | 0x10; // sleep
-        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
-        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
-        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
-        control.waitMicros(5000);
-        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
-    }
-
-    function setPwm(channel: number, on: number, off: number): void {
-        if (channel < 0 || channel > 15)
-            return;
-
-        let buf = pins.createBuffer(5);
-        buf[0] = LED0_ON_L + 4 * channel;
-        buf[1] = on & 0xff;
-        buf[2] = (on >> 8) & 0xff;
-        buf[3] = off & 0xff;
-        buf[4] = (off >> 8) & 0xff;
-        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    export enum OpenMVPort {
+        A1,
+        A2
     }
 
 
-    function setStepper(index: number, dir: boolean): void {
-        if (index == 1) {
-            if (dir) {
-                setPwm(0, STP_CHA_L, STP_CHA_H);
-                setPwm(2, STP_CHB_L, STP_CHB_H);
-                setPwm(1, STP_CHC_L, STP_CHC_H);
-                setPwm(3, STP_CHD_L, STP_CHD_H);
-            } else {
-                setPwm(3, STP_CHA_L, STP_CHA_H);
-                setPwm(1, STP_CHB_L, STP_CHB_H);
-                setPwm(2, STP_CHC_L, STP_CHC_H);
-                setPwm(0, STP_CHD_L, STP_CHD_H);
-            }
-        } else {
-            if (dir) {
-                setPwm(4, STP_CHA_L, STP_CHA_H);
-                setPwm(6, STP_CHB_L, STP_CHB_H);
-                setPwm(5, STP_CHC_L, STP_CHC_H);
-                setPwm(7, STP_CHD_L, STP_CHD_H);
-            } else {
-                setPwm(7, STP_CHA_L, STP_CHA_H);
-                setPwm(5, STP_CHB_L, STP_CHB_H);
-                setPwm(6, STP_CHC_L, STP_CHC_H);
-                setPwm(4, STP_CHD_L, STP_CHD_H);
-            }
-        }
-    }
-
-    function stopMotor(index: number) {
-        setPwm((index - 1) * 2, 0, 0);
-        setPwm((index - 1) * 2 + 1, 0, 0);
-    }
-
-
-
-    /**
-     * Servo Execute
-     * @param index Servo Channel; eg: S1
-     * @param degree [0-180] degree of servo; eg: 0, 90, 180
-    */
-    //% blockId=imagimaker_servo block="Servo %index|degree %degree"
+    //% blockId=imagimaker_initOpenMV block="InitOpenMV port %port|band rate %bandrate"
     //% weight=100
-    //% degree.min=0 degree.max=180
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function Servo(index: Servos, degree: number): void {
-        if (!initialized) {
-            initPCA9685()
+    export function InitOpenMV(port:OpenMVPort): void  {
+
+        Switch(port){
+            case OpenMVPort.A1:
+                serial.redirect(SerialPin.P2,SerialPin.P1, BaudRate.BaudRate19200);
+                break;
+            case OpenMVPort.A2:
+                serial.redirect(SerialPin.P1,SerialPin.P0, BaudRate.BaudRate19200);
+                break;
         }
-        // 50hz: 20,000 us
-        let v_us = (degree * 1800 / 180 + 600) // 0.6 ~ 2.4
-        let value = v_us * 4096 / 20000
-        setPwm(index + 7, 0, value)
     }
     
-    //% blockId=imagimaker_stepper_degree block="Stepper %index|degree %degree"
+    //% blockId=imagimaker_newCategory block="new a category"
     //% weight=90
-    export function StepperDegree(index: Steppers, degree: number): void {
-        if (!initialized) {
-            initPCA9685()
-        }
-        setStepper(index, degree > 0);
-        degree = Math.abs(degree);
-        basic.pause(10240 * degree / 360);
-        MotorStopAll()
+    export function NewCategory(foldName:string): void {
+        serial.writeLine("00001**"+foldName+"**0000**00001**0000000000000000000000000000000000000000000000000000000000000000000");
     }
-
-
-    //% blockId=imagimaker_stepper_turn block="Stepper %index|turn %turn"
-    //% weight=90
-    export function StepperTurn(index: Steppers, turn: Turns): void {
-        let degree = turn;
-        StepperDegree(index, degree);
-    }    
-
-    function _MotorRun(index: Motors ,speed: number): void {
-        if (!initialized) {
-            initPCA9685()
-        }
-        speed = speed * 4; // map 255 to 4096
-        if (speed >= 4096) {
-            speed = 4095
-        }
-        if (speed <= -4096) {
-            speed = -4095
-        }
-        if (index > 4 || index <= 0)
-            return
-        let pp = (index - 1) * 2
-        let pn = (index - 1) * 2 + 1
-        if (speed >= 0) {
-            setPwm(pp, 0, speed)
-            setPwm(pn, 0, 0)
-        } else {
-            setPwm(pp, 0, 0)
-            setPwm(pn, 0, -speed)
-        }
-    }
-
-    //% blockId=imagimaker_motor_run block="Motor %index|running with direction %direction| and speed %speed"
-    //% weight=85
-    //% speed.min=0 speed.max=1024
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function MotorRun(index: Motors, direction: Directions, speed: number): void {
-        let signedSpeed = speed * ( direction > 0 ? 1 : -1 );
-        _MotorRun(index, signedSpeed);
-    }
-
-
-    //% blockId=imagimaker_stop block="Motor Stop %index"
+    //% blockId=imagimaker_viewCategoryCnt block="view quality of category"
     //% weight=80
-    export function MotorStop(index: Motors): void {
-        _MotorRun(index, 0);
+    export function ViewCategoryCnt() {
+        serial.writeLine("00010**000000000000**0000**00001**000000000000000000000000000000000000000000000000000000000000000000")
+    }
+    //% blockId=imagimaker_viewIMGCntUnderFold_Name block="view img"
+    //% weight=70
+    export function ViewIMGCntUnderFold_Name(foldName:string): void {
+        serial.writeLine("00011**"+foldName+"**0000**00001**0000000000000000000000000000000000000000000000000000000000000000000");
+    } 
+    //% blockId=imagimaker_viewIMGCntUnderFold_Name block="view img"
+    //% weight=60       
+    export function TakePhotos(picName:string,foldName:string) {
+        serial.writeLine("00100**"+foldName+"**"+picName+"**00001**000000000000000000000000000000000000000000000000000000000000000");
+    }
+    //% blockId=imagimaker_recognitionCurrIMG block="recognition current image"
+    //% weight=50
+    export function RecognitionCurrIMG() {
+        serial.writeLine("10000**000000000000**0000**00001**000000000000000000000000000000000000000000000000000000000000000000");
     }
 
-    //% blockId=imagimaker_stop_all block="Motor Stop All"
-    //% weight=79
-    //% blockGap=50
-    export function MotorStopAll(): void {
-        for (let idx = 1; idx <= 4; idx++) {
-            stopMotor(idx);
-        }
+    //% blockId=imagimaker_clearSDcard block="clear SD card"
+    //% weight=40
+    export function ClearSDcard() {
+        serial.writeLine("01000**000000000000**0000**00001**000000000000000000000000000000000000000000000000000000000000000000");
     }
-
 }
